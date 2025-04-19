bac48833ef3b103a05371ee0a2fa2a2a87dc0383
Kerem YEREBAKAN <keremeyrbkn@gmail.com>
2025-04-19 23:13:06 +0300

    Hakkımızda Sayfası Tasarlandı

---
+++ DoTechSoftMVC/Controllers/GuestAboutController.cs
@@ -0,0 +1,22 @@
+﻿using DoTechSoftMVC.Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DoTechSoftMVC.Controllers
+{
+    public class GuestAboutController : Controller
+    {
+        private readonly IAboutRepository _aboutRepository;
+        public GuestAboutController(IAboutRepository aboutRepository)
+        {
+            _aboutRepository = aboutRepository;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var abouts = await _aboutRepository.GetAllAsync();
+            var about = abouts.FirstOrDefault();
+            return View(about);
+        }
+    }
+}
